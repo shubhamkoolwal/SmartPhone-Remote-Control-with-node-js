fb1deabb98b33e54a6f5eb96b94dc38110f434bc
shubhamkoolwal <shubham.koolwal121@gmail.com>
2017-10-15 12:36:07 +0530

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,44 +11,17 @@ $(function() {
 	var socket = io();
 
 	// Variable initialization
-
-//	var form = $('form.login');
-//	var secretTextBox = form.find('input[type=text]');
 	var presentation = $('.reveal');
 
 	var  animationTimeout;
 
-	// When the page is loaded it asks you for a key and sends it to the server
-
-/*	form.submit(function(e){
-
-		e.preventDefault();
-
-		key = secretTextBox.val().trim();
-
-		// If there is a key, send it to the server-side
-		// through the socket.io channel with a 'load' event.
-
-		if(key.length) {
-			socket.emit('load', {
-				key: key
-			});
-		}
-
-	});
-
-*/	// The server will either grant or deny access, depending on the secret key
-
+	
 	socket.on('connect', function(data){
 
-		// Check if we have "granted" access.
 		// If we do, we can continue with the presentation.
           console.log("connected to the server");
 		
-			// Unblur everything
-			//presentation.removeClass('blurred');
-
-			//form.hide();
+			
 
 			var ignore = false;
 
@@ -87,26 +60,6 @@ $(function() {
 				},100);
 
 			});
-/*
-		
-		else 
-		 {
-
-			// Wrong secret key
-
-			clearTimeout(animationTimeout);
-
-			// Addding the "animation" class triggers the CSS keyframe
-			// animation that shakes the text input.
-
-			secretTextBox.addClass('denied animation');
-			
-			animationTimeout = setTimeout(function(){
-				secretTextBox.removeClass('animation');
-			}, 1000);
-
-			form.show();
-		}*/
 
 	});
 
